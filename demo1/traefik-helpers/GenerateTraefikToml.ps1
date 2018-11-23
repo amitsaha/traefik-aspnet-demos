ec280af16776620d210293175bf9348a953b0567
Amit Saha <amitsaha@users.noreply.github.com>
2018-11-23 17:09:42 +1100

    Update GenerateTraefikToml.ps1

--- demo1/traefik-helpers/GenerateTraefikToml.ps1
+++ demo1/traefik-helpers/GenerateTraefikToml.ps1
@@ -1,10 +1,19 @@
 $template=Get-Content "C:\traefik\rules.toml.template" -Encoding UTF8 -Raw
-$site1=docker ps --filter "health=healthy" --filter "label=app=apsnetframwork-demo" --format '{{.ID}}'
+$site1=docker ps --filter "health=healthy" --filter "label=app=apsnetframwork-demo-site1" --format '{{.ID}}'
 if ($site1)
 {
-    $siteIP=docker inspect $site1 --format '{{ .NetworkSettings.Networks.nat.IPAddress}}'
+    $apiIP=docker inspect $site1 --format '{{ .NetworkSettings.Networks.nat.IPAddress}}'
 } else {
     Write-Output "--- Site1 Container not ready"
+    exit 1
+}
+
+$site2=docker ps --filter "health=healthy" --filter "label=app=apsnetframwork-demo-site2" --format '{{.ID}}'
+if ($site1)
+{
+    $adminIP=docker inspect $site2 --format '{{ .NetworkSettings.Networks.nat.IPAddress}}'
+} else {
+    Write-Output "--- Site2 Container not ready"
     exit 1
 }
 
