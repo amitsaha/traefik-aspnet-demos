f299e28bf25787591afe74b14d3a4b486a7968a6
Amit Saha <amitsaha.in@gmail.com>
2018-11-28 04:06:06 +0000

    update

--- demo1/DeployScripts/RunSites.ps1
+++ demo1/DeployScripts/RunSites.ps1
@@ -1,7 +1,21 @@
+param(     
+     [Parameter(Mandatory=$True)][string]$GitHash
+)
+
 $ApplicationName = "aspnetframework-demo-site1"
-$Image="amitsaha/aspnetframework-demo-site1"
-docker run --label "app=${ApplicationName}" -d $Image
+$Image="amitsaha/aspnetframework-demo-site1:$GitHash"
+docker run `
+      --label "traefik.backend=site1" `
+      --label "traefik.frontend.rule=Host:site1.echorand.me"  `
+      --label "traefik.port=80" `
+      --label "app=${ApplicationName}" `
+      --label "version=$GitHash" -d $Image
 
 $ApplicationName = "aspnetframework-demo-site2"
-$Image="amitsaha/aspnetframework-demo-site2"
-docker run --label "app=${ApplicationName}" -d $Image
+$Image="amitsaha/aspnetframework-demo-site2:$GitHash"
+docker run `
+      --label "traefik.backend=site2" `
+      --label "traefik.frontend.rule=Host:site2.echorand.me"  `
+      --label "traefik.port=80" `
+      --label "app=${ApplicationName}" `
+      --label "version=$GitHash" -d $Image
