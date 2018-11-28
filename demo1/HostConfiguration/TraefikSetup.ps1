315c46bd4e46ab5a9d786601d020178f405eb7aa
Amit Saha <amitsaha@users.noreply.github.com>
2018-11-28 20:56:10 +1100

    Update TraefikSetup.ps1

--- demo1/HostConfiguration/TraefikSetup.ps1
+++ demo1/HostConfiguration/TraefikSetup.ps1
@@ -1,17 +1,8 @@
-param(
-     [string]$TraefikTomlPath     
-)
-# Example invocation:
-# .\TraefikSetup.ps1 -TraefikTomlPath C:\data\traefik\traefik.toml -RulesTemplateTomlPath C:\data\traefik\rules.toml.template
 
 $TraefikHome = "C:\Traefik"
 $TraefikRelease = "v1.7.4"
 $NssmHome = "C:\nssm"
 $NssmRelease = "2.24"
-
-if (-Not (Test-Path $TraefikTomlPath)) {
-    throw "$TraefikTomlPath not found"
-}
 
 
 # Use TLS 1.2
@@ -27,7 +18,7 @@ if (-Not (Test-Path "$TraefikHome\traefik.exe"))
     Invoke-WebRequest "https://github.com/containous/traefik/releases/download/$TraefikRelease/traefik_windows-amd64.exe" -OutFile "$TraefikHome\traefik.exe"
 }
 
-Copy-Item $TraefikTomlPath $TraefikHome\traefik.toml -force
+Copy-Item traefik-helpers\traefik.toml $TraefikHome\traefik.toml -force
 
 # Download NSSM
 if (-Not (Test-Path $NssmHome)) {
