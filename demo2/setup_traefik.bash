24728e8fe805b2044dbd7b0734ac123c149320f6
Amit Saha <amitsaha@users.noreply.github.com>
2018-12-15 17:11:52 +1100

    Update setup_traefik.bash

--- demo2/setup_traefik.bash
+++ demo2/setup_traefik.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+chmod 0666 acme.json
 sudo docker run -d -p 80:80 -p 443:443 \
         -v $PWD/traefik.toml:/etc/traefik/traefik.toml \
         -v /var/run/docker.sock:/var/run/docker.sock \
